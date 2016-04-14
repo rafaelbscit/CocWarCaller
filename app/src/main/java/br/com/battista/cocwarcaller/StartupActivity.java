17a1c1c5c7ae685a3664c1995cc0ffc1c91296d0
Rafael Batista <rabsouza@gmail.com>
2016-04-14 05:19:24 -0300

    [rafaelbs] Add activity About and menu app

--- app/src/main/java/br/com/battista/cocwarcaller/StartupActivity.java
+++ app/src/main/java/br/com/battista/cocwarcaller/StartupActivity.java
@@ -1,7 +1,10 @@
 package br.com.battista.cocwarcaller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class StartupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void openMainActivity(View view) {
+        startActivity(new Intent(this, MainActivity.class), null);
     }
 }
